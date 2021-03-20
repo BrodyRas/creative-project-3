4be5e38517329821f5dabb8294747549e1982633
Brody Rasmussen <brodyras@gmail.com>
2021-03-20 14:03:39 -0600

    added deleteLog functionality

--- src/components/LogCard.vue
+++ src/components/LogCard.vue
@@ -13,7 +13,12 @@
           </li>
         </ul>
       </div>
-      <p><i>Written on {{Date.parse(log.written)}}</i></p>
+      <div class="bottom-row">
+        <p>
+          <i>Written on {{ readableDate }}</i>
+        </p>
+        <button v-on:click="deleteLog">Delete?</button>
+      </div>
     </div>
   </div>
 </template>
@@ -24,7 +29,38 @@ export default {
   props: {
     log: Object,
   },
-  methods: {},
+  methods: {
+    deleteLog(){
+      let index = this.$root.$data.logs.indexOf(this.log);
+      this.$root.$data.logs.splice(index, 1);
+    }
+  },
+  computed: {
+    readableDate() {
+      let a = new Date(this.log.written);
+      let months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ];
+      let year = a.getFullYear();
+      let month = months[a.getMonth()];
+      let date = a.getDate();
+      let hour = a.getHours();
+      let min = a.getMinutes();
+      let time = date + " " + month + " " + year + " " + hour + ":" + min;
+      return time;
+    },
+  },
 };
 </script>
 
@@ -41,5 +77,24 @@ export default {
   background-color: rgb(248, 240, 240);
   border-radius: 10px;
   padding: 10px;
+}
+
+.bottom-row{
+  display: flex;
+  justify-content: space-between;
+}
+
+.bottom-row button {
+  background-color: rgb(129, 57, 69);
+  color: white;
+  font-weight: bold;
+  border: none;
+  border-radius: 10px;
+  margin-top: 5px;
+  transition: all 0.3s;
+}
+
+.bottom-row button:hover{
+  font-size: 1.05em;
 }
 </style>
